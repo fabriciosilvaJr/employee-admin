d5ef57d52d56c9c1209f8a27c1cf65385c6c461c
fabriciosilvaJr <fabriciosilva187@gmail.com>
2025-11-02 14:51:28 -0300

    adição do codeIgniter 3

---
+++ sql/migrate.sql
@@ -0,0 +1,29 @@
+
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO users (name, email, password)
+VALUES (
+    'Administrador',
+    'admin@example.com',
+    crypt('123456', gen_salt('bf'))
+);
+
+
+CREATE TABLE IF NOT EXISTS employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    position VARCHAR(50) NOT NULL,
+    salary NUMERIC(10,2) NOT NULL,
+    admission_date DATE NOT NULL
+);
